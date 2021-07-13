2528c85648db5d0dd73008b31dd088f5a38d8441
Morgo <morgosusin@gmail.com>
2021-07-13 16:01:26 +0200

    Add digital clock project base

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+import * as util from "../common/utils";
+
+// Update the clock every minute
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+  }
+  let mins = util.zeroPad(today.getMinutes());
+  myLabel.text = `${hours}:${mins}`;
+}
