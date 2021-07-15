761367d28e532e1db4a1f2503a34ce95c6bb4d1d
Morgo <morgosusin@gmail.com>
2021-07-15 06:24:22 +0200

    Refactor existing code

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
-import * as util from "../common/utils";
+import * as util from "./simple/utils";
 
 import * as messaging from "messaging"; //settings
 
@@ -14,16 +14,12 @@ import { BodyPresenceSensor } from "body-presence"; //onWrist
 import { me as appbit } from "appbit"; //part of using today
 import { today } from "user-activity"; //steps, elevation, goals, ...
 import { user } from "user-profile"; //resting heart rate, gender, age, bmr, stride, weight, height, ...
-  //also heartRateZone: Returns: "out-of-range" or "fat-burn" or "cardio" or "peak" or "below-custom" or "custom" or "above-custom"
-
-//GPS
-//import { geolocation } from "geolocation";
+//also heartRateZone: Returns: "out-of-range" or "fat-burn" or "cardio" or "peak" or "below-custom" or "custom" or "above-custom"
 
 //Battery
 import { battery } from "power";
+import { memory } from "system";
 
-// Update the clock every minute
-clock.granularity = "minutes";
 
 const Console = document.getElementById("console");
 
@@ -37,44 +33,40 @@ const vaultBoy = document.getElementById("vault-boy");
 
 const dweller = document.getElementById("dweller");
 
-//const latitude = document.getElementById("latitude");
-//const longitude = document.getElementById("longitude");
-//const heading = document.getElementById("heading");
-
 const lvl = document.getElementById("lvl");
 
-const steps = document.getElementById("steps");
-const minutes = document.getElementById("minutes");
-const burn = document.getElementById("burn");
-const distance = document.getElementById("distance");
-const elevation = document.getElementById("elevation");
-const rhr = document.getElementById("resting");
-const wt = document.getElementById("weight");
+const hrmData = document.getElementById("heart");
 
 const bars = document.getElementsByClassName("v");
 
-let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// Update the clock every minute
+clock.granularity = "seconds";
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => { //Todo: add sensors to ticking? is that even needed?
   let today = evt.date;
   let hours = today.getHours();
-  
+
   hours = util.zeroPad(hours);
-  
+
   let mins = util.zeroPad(today.getMinutes());
   clockLabel.text = `${hours}:${mins}`;
-  
-  
+
+
   //TODO: Does this need to be within a tick? It only needs to update once a day at midnight...
   let months = util.zeroPad(today.getMonth()+1);
   let days = util.zeroPad(today.getDate());
-  
-  let date = today.getFullYear() + "-" + months + "-" + days + " " + dayNames[today.getDay()];
-  dateLabel.text = date;  
-  
-  /*--- Battery charging ---*/
-  lvl.width = battery.chargeLevel*1.54;
+
+  let day = dayNames[today.getDay()];
+  let year = today.getFullYear();
+
+  let date = day + " " + year + "-" + months + "-" + days;
+
+  dateLabel.text = date;
+
+  console.log("JS memory: " + memory.js.used + "/" + memory.js.total + ", peak: " + memory.js.peak + ", pressure: " + memory.js.pressure);
 }
 
 /*--- Settings ---*/
@@ -91,57 +83,31 @@ messaging.peerSocket.onmessage = evt => {
   }
 };
 
-// Message socket opens
-messaging.peerSocket.onopen = () => {
-  //console.log("App Socket Open");
-};
-
-// Message socket closes
-messaging.peerSocket.onclose = () => {
-  //console.log("App Socket Closed");
-};
-
 /*--- Sensors ---*/
 
 const sensors = [];
 
-if (appbit.permissions.granted("access_heart_rate")) {
-  const hrmData = document.getElementById("heart");
+//TODO: bodyPresence.present
+const hrm = new HeartRateSensor({ frequency: 1 });
+hrm.addEventListener("reading", () => {
+  hrmData.text = hrm.heartRate ? hrm.heartRate : 0;
+});
+sensors.push(hrm);
 
-  const hrm = new HeartRateSensor({ frequency: 1 });
-  hrm.addEventListener("reading", () => {
-    hrmData.text = hrm.heartRate ? hrm.heartRate : 0;
-  });
-  sensors.push(hrm);
-  hrm.start();
-}
-
-//Todo: Condition this with onWrist
-if (appbit.permissions.granted("access_activity")) {
-  /*--- Today ---*/  
-  steps.text = today.adjusted.steps;
-  minutes.text = today.adjusted.activeZoneMinutes.total;
-  burn.text = today.adjusted.calories;
-  distance.text = (today.adjusted.distance/1000).toFixed(2);
-  elevation.text = today.adjusted.elevationGain;
-  
-  rhr.text = "HP " + user.restingHeartRate;
-  wt.text = user.weight + " KG";
-}
-
-
+hrm.start();
 
 /*--- Is the watch on a creature? ---*/
 
 const bodyPresence = new BodyPresenceSensor();
+
 bodyPresence.addEventListener("reading", () => {
   let i;
-  
+
   if(bodyPresence.present) {
     for (i = 0; i < bars.length; i++) {
       bars[i].style.opacity = 1;
     }
-    
+
     if(battery.charging) {
       Console.text = "> CHARGING ..."
     } else {
@@ -151,7 +117,7 @@ bodyPresence.addEventListener("reading", () => {
     for (i = 0; i < bars.length; i++) {
       bars[i].style.opacity = 0;
     }
-    
+
     if(battery.charging) {
       Console.text = "> CHARGING ..."
     } else {
@@ -160,39 +126,66 @@ bodyPresence.addEventListener("reading", () => {
   }
 
 });
+
 sensors.push(bodyPresence);
 bodyPresence.start();
+
+//Todo: Condition this with onWrist
+/*--- Today ---*/
+function updateStats() {
+  const steps = document.getElementById("steps");
+  const minutes = document.getElementById("minutes");
+  const burn = document.getElementById("burn");
+  const distance = document.getElementById("distance");
+  const elevation = document.getElementById("elevation");
+  const rhr = document.getElementById("resting");
+  const wt = document.getElementById("weight");
+
+  steps.text = today.adjusted.steps;
+  minutes.text = today.adjusted.activeZoneMinutes.total;
+  burn.text = today.adjusted.calories;
+  distance.text = (today.adjusted.distance/1000).toFixed(2);
+  elevation.text = today.adjusted.elevationGain;
+
+  rhr.text = "HP " + user.restingHeartRate;
+  wt.text = user.weight + " KG";
+  console.log("Stats updated.");
+} updateStats();
 
 /*--- Display ---*/
 display.addEventListener("change", () => {
   // Probably better to run these things once when the screen turns on than every second...
   if(display.on) {
+    console.log("Display on.");
     wake();
   } else {
+    console.log("Display off.");
     sleep();
   }
 });
 
 function wake() {
-  console.log("Display is on.");
-  sensors.map(sensor => sensor.start()); //sensors
+  turnOn(sensors);
   updateStats();
 }
 
-function updateStats() {
-  
+function sleep() {
+  turnOff(sensors);
 }
 
-function sleep() {
-  console.log("Display is off.");  
-  sensors.map(sensor => sensor.stop()); //sensors
+function turnOn(callback) {
+  callback.map(sensor => sensor.start());
+}
+
+function turnOff(callback) {
+  callback.map(sensor => sensor.stop());
 }
 
 battery.addEventListener("change", () => {
   // Battery changes
-  
+
   lvl.width = battery.chargeLevel*1.54;
-  
+
   if(battery.charging) {
     Console.text = "> CHARGING ...";
   } else if(!bodyPresence.present) {
