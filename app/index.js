0043d6314e86e746621763c58db78997fa78d59d
Morgo <morgosusin@gmail.com>
2021-07-17 05:47:45 +0200

    Move clock to clock.js, Remove passing hours to console every second, do it on demand

--- app/index.js
+++ app/index.js
@@ -1,97 +1,24 @@
-//TODO: Add 'No user' on unequipping the watch (not on body)
-//TODO: Refactor / Separate
-
-//TODO: Battery efficiency - do some stuff only if there's enough battery left, reorganize when it reaches ... 20?
-
-import clock from "clock";
 import document from "document";
-import { preferences } from "user-settings";
-import * as util from "./simple/utils";
 
-import * as messaging from "messaging"; //settings
+import * as messaging from "messaging";
 
-//Battery
-import { memory } from "system";
-
-import * as Console from "./simple/console";
 import * as stats from "./simple/stats";
 import * as sensors from "./simple/sensors";
-
-const clockLabel = document.getElementById("t");
-const dateLabel = document.getElementById("d");
-
-
-/*--- Clock ---*/
-
-const cursorStop = false;
-
-let hours = 12;
-
-// Update the clock every minute
-clock.granularity = "seconds";
-
-let previousDay = 8;
-// Update the <text> element every tick with the current time
-clock.ontick = (evt) => { //Todo: add sensors to ticking? is that even needed?
-  let today = evt.date;
-  
-  hours =  util.zeroPad(today.getHours());
-  
-  Console.passHour(hours);
-  
-  let mins = util.zeroPad(today.getMinutes());
-  let secs = util.zeroPad(today.getSeconds());
-  
-  clockLabel.text = `${hours}:${mins}`;
-
-  let dayNumber = 9;
-  dayNumber = today.getDay();
-  
-  if(previousDay !== dayNumber) {
-    const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    
-    dayNumber = today.getDay();
-    
-    //it only really needs to happen once a day at midnight and during startup...
-    console.log("Date udated.")
-    let months = util.zeroPad(today.getMonth()+1);
-  
-    let days = util.zeroPad(today.getDate());
-    let day = dayNames[dayNumber];
-    let year = today.getFullYear();
-
-    let date = `${day}   ${year}-${months}-${days}`;
-  
-    dateLabel.text = date;
-    
-    previousDay = dayNumber;
-    
-    let date, year, day, days, months, dayNumber, dayNames = null;
-  }
-
-  if(!cursorStop)
-    Console.tick();
-
-  console.log(`JS memory: ${memory.js.used} / ${memory.js.total}, peak: ${memory.js.peak}.`);
-}
+import * as clock from "./simple/clock";
+import * as misc from "./simple/misc";
 
 /*--- Settings ---*/
-const background = document.getElementById("b");
-const dweller = document.getElementById("o");
-
 messaging.peerSocket.onmessage = evt => {
   if (evt.data.key === "color" && evt.data.newValue)
-    background.style.fill = JSON.parse(evt.data.newValue);
+    document.getElementById("b").style.fill = JSON.parse(evt.data.newValue);
   else if (evt.data.key === "name" && evt.data.newValue)
-    dweller.text = JSON.parse(evt.data.newValue).name;
+    document.getElementById("o").text = JSON.parse(evt.data.newValue).name;
   else if (evt.data.key === "cursor")
-    cursorStop = JSON.parse(evt.data.newValue);
+    clock.importCursorSettings(JSON.parse(evt.data.newValue));
 };
 
-/*--- Sensors ---*/
+clock.init();
+
 sensors.init();
 
-/*--- Stats ---*/
 stats.update();
-
-
---
+++ app/simple/clock.js
@@ -0,0 +1,73 @@
+import document from "document";
+
+import clock from "clock";
+
+import * as Console from "./console";
+import * as memory from "./memory";
+
+clock.granularity = "seconds";
+
+/*--- Misc ---*/
+function zeroPad(i) {
+  if (i < 10) {
+      i = "0" + i;
+  }
+  return i;
+}
+
+/*--- Clock ---*/
+
+let hour = 0;
+
+export function getHour() {
+  return hour;
+}
+
+let previousDay = 8;
+
+const clockLabel = document.getElementById("t");
+let cursorStop = false;
+
+export function importCursorSettings(newValue) {
+  cursorStop = newValue;
+}
+
+export function init() {
+  clock.ontick = (evt) => {
+    let today = evt.date;
+    hour = today.getHours();
+    let hours =  zeroPad(hour);
+    let mins = zeroPad(today.getMinutes());
+    let secs = zeroPad(today.getSeconds());
+  
+    clockLabel.text = `${hours}:${mins}`;
+  
+    if(previousDay !== today.getDay())
+      updateDate(today, previousDay);
+    
+    if(!cursorStop)
+      Console.tick();
+  }
+}
+
+function updateClock() {
+  
+}
+
+function updateDate(today, previousDayNumber) {
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let dayNumber = today.getDay();
+  let dayOfWeek = dayNames[dayNumber];
+  let dayOfMonth = zeroPad(today.getDate());
+  let month = zeroPad(today.getMonth()+1);
+  let year = today.getFullYear();
+
+  let date = `${dayOfWeek}   ${year}-${month}-${dayOfMonth}`;
+  
+  document.getElementById("d").text = date;
+    
+  previousDay = dayNumber;
+    
+  let date, year, day, days, months, dayNumber, dayNames = null;
+}
